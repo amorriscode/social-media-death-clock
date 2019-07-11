30609a1d974c6ca5f9fdb22560e61f398a363f3a
Anthony Morris <anthony@amorrissound.com>
2019-07-10 17:55:03 -0700

    Make the clock show up sooner

--- death-clock.js
+++ death-clock.js
@@ -35,81 +35,89 @@ const socialMediaTargetClasses = {
   none: '.insert-death-clock'
 };
 
+const getBirthdate = () => {
+  chrome.storage.sync.get(['birthdate'], ({ birthdate }) => {
+    if (birthdate) {
+      [birthYear, birthMonth, birthDay] = birthdate.split('-');
+    }
+  });
+}
+
+const addCommansToNumber = (number) => {
+  const reverseNumber = number
+    .toString()
+    .split('')
+    .reverse();
+
+  if (reverseNumber.length > 3) {
+    for (i = 3; i < reverseNumber.length; i += 3) {
+      reverseNumber[i] = `${reverseNumber[i]},`
+    }
+  }
+
+  return reverseNumber.reverse().join('');
+}
+
+const tickTock = () => {
+  // Check for birthdate every time incase it has been updated
+  getBirthdate();
+
+  // JavaScript months start at 0
+  const birthDate = new Date(birthYear, birthMonth - 1, birthDay);
+  const expectedDeath = new Date(birthDate.getFullYear() + AVERAGE_HUMAN_EXPIRY_AGE, birthDate.getMonth(), birthDate.getDate());
+
+  let deathClock = (socialMediaPlatform !== 'none') ? '<div>SET YOUR BIRTHDATE!</div>' : '';
+
+  if (birthYear && birthDay && birthMonth) {
+    const now = new Date();
+    const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    const nextHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1);
+    const nextMinute = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 1);
+    
+    const timeUntilTomorrow = Math.abs(now.getTime() - tomorrow.getTime());
+    const timeUntilNextHour = Math.abs(now.getTime() - nextHour.getTime());
+    const timeUntilNextMinute = Math.abs(now.getTime() - nextMinute.getTime());
+    const timeUntilExpiry = Math.abs(expectedDeath.getTime() - now.getTime());
+  
+    const daysUntilExpiry = Math.ceil(timeUntilExpiry / DAYS_IN_MILLISECONDS);
+    let hoursUntilTomorrow = Math.ceil(timeUntilTomorrow / HOURS_IN_MILLISECONDS);
+    hoursUntilTomorrow = (hoursUntilTomorrow.toString().length === 1) ? `0${hoursUntilTomorrow}` : hoursUntilTomorrow;
+    const minutesUntilNextHour = Math.ceil(timeUntilNextHour / MINUTES_IN_MILLISECONDS - 1);
+    const secondsUntilNextMinute = Math.ceil(timeUntilNextMinute / SECONDS_IN_MILLISECONDS) - 1;
+
+    const daysToDisplay = addCommansToNumber(daysUntilExpiry);
+    const secondsToDisplay = (secondsUntilNextMinute.toString().length === 1)
+      ? `0${secondsUntilNextMinute}`
+      : secondsUntilNextMinute; 
+    
+    deathClock = `
+      <div class="days-until-expiry">${daysToDisplay} days</div>
+      <div class="time-until-expiry">${hoursUntilTomorrow}:${minutesUntilNextHour}:${secondsToDisplay}</div>
+    `;
+  }
+
+  // Chrome Extension popup doesn't like emojis
+  const skull = (socialMediaPlatform !== 'none') ? '☠️' : '';
+
+  target.innerHTML = `
+      <div class="social-media-death-clock">
+        <div class="skull-emoji">${skull}</div>
+        <div class="death-clock">
+          ${deathClock}
+        </div>
+      </div>
+    `;
+}
+
 const startDeathClock = () => {
   target.id = `death-clock-${socialMediaPlatform}`;
-
-  const addCommansToNumber = (number) => {
-    const reverseNumber = number
-      .toString()
-      .split('')
-      .reverse();
   
-    if (reverseNumber.length > 3) {
-      for (i = 3; i < reverseNumber.length; i += 3) {
-        reverseNumber[i] = `${reverseNumber[i]},`
-      }
-    }
-  
-    return reverseNumber.reverse().join('');
-  }
-  
-  const tickTock = () => {
-    // Check for birthdate every time incase it has been updated
-    chrome.storage.sync.get(['birthdate'], ({ birthdate }) => {
-      if (birthdate) {
-        [birthYear, birthMonth, birthDay] = birthdate.split('-');
-      }
-    });
-
-    // JavaScript months start at 0
-    const birthDate = new Date(birthYear, birthMonth - 1, birthDay);
-    const expectedDeath = new Date(birthDate.getFullYear() + AVERAGE_HUMAN_EXPIRY_AGE, birthDate.getMonth(), birthDate.getDate());
-
-    let deathClock = (socialMediaPlatform !== 'none') ? '<div>SET YOUR BIRTHDATE!</div>' : '';
-
-    if (birthYear && birthDay && birthMonth) {
-      const now = new Date();
-      const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-      const nextHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1);
-      const nextMinute = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 1);
-      
-      const timeUntilTomorrow = Math.abs(now.getTime() - tomorrow.getTime());
-      const timeUntilNextHour = Math.abs(now.getTime() - nextHour.getTime());
-      const timeUntilNextMinute = Math.abs(now.getTime() - nextMinute.getTime());
-      const timeUntilExpiry = Math.abs(expectedDeath.getTime() - now.getTime());
-    
-      const daysUntilExpiry = Math.ceil(timeUntilExpiry / DAYS_IN_MILLISECONDS);
-      let hoursUntilTomorrow = Math.ceil(timeUntilTomorrow / HOURS_IN_MILLISECONDS);
-      hoursUntilTomorrow = (hoursUntilTomorrow.toString().length === 1) ? `0${hoursUntilTomorrow}` : hoursUntilTomorrow;
-      const minutesUntilNextHour = Math.ceil(timeUntilNextHour / MINUTES_IN_MILLISECONDS - 1);
-      const secondsUntilNextMinute = Math.ceil(timeUntilNextMinute / SECONDS_IN_MILLISECONDS) - 1;
-    
-      const daysToDisplay = addCommansToNumber(daysUntilExpiry);
-      const secondsToDisplay = (secondsUntilNextMinute.toString().length === 1)
-        ? `0${secondsUntilNextMinute}`
-        : secondsUntilNextMinute; 
-      
-      deathClock = `
-        <div class="days-until-expiry">${daysToDisplay} days</div>
-        <div class="time-until-expiry">${hoursUntilTomorrow}:${minutesUntilNextHour}:${secondsToDisplay}</div>
-      `;
-    }
-
-    // Chrome Extension popup doesn't like emojis
-    const skull = (socialMediaPlatform !== 'none') ? '☠️' : '';
-
-    target.innerHTML = `
-        <div class="social-media-death-clock">
-          <div class="skull-emoji">${skull}</div>
-          <div class="death-clock">
-            ${deathClock}
-          </div>
-        </div>
-      `;
-  }
-
+  // Run the clock once then start the interval
+  tickTock();
   window.setInterval(() => tickTock(), 1000);
 }
+
+getBirthdate();
 
 let target;
 const findTarget = setInterval(() => {
@@ -119,4 +127,3 @@ const findTarget = setInterval(() => {
     clearInterval(findTarget);
   }
 }, 50);
-

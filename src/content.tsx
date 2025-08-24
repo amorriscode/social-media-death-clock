7b0245ddee589f4a0aac1961ec8f8fabc931d010
Anthony Morris <amorriscode@gmail.com>
2025-08-23 18:18:41 -0700

    feat: animation

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,8 @@
+import clsx from "clsx"
 import skull from "data-base64:~/../assets/skull.png"
 import css from "data-text:~styles.css"
 import type { PlasmoCSConfig } from "plasmo"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { useDeathClock } from "~/useDeathClock"
 
@@ -38,21 +39,30 @@ export const getRootContainer = async () => {
 }
 
 export default function DeathClockBanner() {
-  const [showTimer, setShowTimer] = useState(true)
+  const [showTimer, setShowTimer] = useState(false)
   const [isHovered, setIsHovered] = useState(false)
   const { birthdate, timeLeft, setBirthdate } = useDeathClock()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowTimer(true)
+    }, 1000)
+  }, [timeLeft])
 
   return (
     <div
       className="fixed bottom-0 left-0 right-0 m-4 font-sans flex justify-center items-center z-[999999999]"
       style={{ pointerEvents: "none" }}>
       <div
-        className={`p-2 ${showTimer ? "px-8" : "px-4"} bg-black text-white rounded-full border border-zinc-800 shadow-2xl cursor-default relative hover:cursor-pointer`}
+        className={clsx(
+          "h-[48px] bg-black text-white rounded-full border border-zinc-800 shadow-2xl cursor-default relative hover:cursor-pointer transition-all duration-700 overflow-hidden flex items-center justify-center",
+          !showTimer ? "w-[48px]" : timeLeft ? "w-[280px]" : "w-[320px]"
+        )}
         style={{ pointerEvents: "auto" }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         onClick={() => timeLeft && setShowTimer((currTimer) => !currTimer)}>
-        {showTimer ? (
+        {showTimer && (
           <>
             {!birthdate && (
               <div className="">
@@ -70,7 +80,10 @@ export default function DeathClockBanner() {
             {timeLeft && (
               <div className="relative">
                 <div
-                  className={`transition-opacity duration-500 ${isHovered ? "opacity-0" : "opacity-100"}`}>
+                  className={clsx(
+                    "transition-opacity duration-500",
+                    isHovered ? "opacity-0" : "opacity-100"
+                  )}>
                   <div className="flex space-x-2 text-center font-mono">
                     <div className="text-sm w-8 inline-block tabular-nums">
                       {timeLeft.yearsLeft}
@@ -95,7 +108,10 @@ export default function DeathClockBanner() {
                 </div>
 
                 <div
-                  className={`absolute inset-0 flex items-center justify-center transition-opacity duration-700 ${isHovered ? "opacity-100" : "opacity-0"}`}>
+                  className={clsx(
+                    "absolute inset-0 flex items-center justify-center transition-opacity duration-700",
+                    isHovered ? "opacity-100" : "opacity-0"
+                  )}>
                   <div className="text-xs tracking-wider uppercase">
                     MAKE IT COUNT
                   </div>
@@ -103,11 +119,16 @@ export default function DeathClockBanner() {
               </div>
             )}
           </>
-        ) : (
-          <div className="cursor-pointer">
-            <img src={skull} alt="Some pretty cool image" className="w-4 h-4" />
-          </div>
         )}
+
+        <img
+          src={skull}
+          alt="Social Media Death Clock skull logo"
+          className={clsx(
+            "w-4 h-4 absolute top-0 right-0 bottom-0 left-0 m-auto transition-opacity duration-700",
+            showTimer ? "opacity-0" : "opacity-100"
+          )}
+        />
       </div>
     </div>
   )
